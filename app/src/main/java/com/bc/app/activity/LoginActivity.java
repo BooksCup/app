09acbec2effc8c120a513b2f03129c44b156c02c
BooksCup <812809680@qq.com>
2021-01-15 18:30:21 +0800

    新增登录页

--- app/src/main/java/com/bc/app/activity/LoginActivity.java
+++ app/src/main/java/com/bc/app/activity/LoginActivity.java
@@ -1,10 +1,8 @@
 package com.bc.app.activity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
 import com.bc.app.R;
@@ -12,9 +10,12 @@ import com.bc.app.R;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
-public class SplashActivity extends FragmentActivity {
-
-    Handler mHandler;
+/**
+ * 登录
+ *
+ * @author zhou
+ */
+public class LoginActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,14 +25,6 @@ public class SplashActivity extends FragmentActivity {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-        setContentView(R.layout.activity_splash);
-
-        mHandler = new Handler();
-        mHandler.postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }, 1500);
+        setContentView(R.layout.activity_login);
     }
-
 }
